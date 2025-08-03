538f882b98e50d5060961a1fe516c18c21d42723
Nathan Kida <nathankida@gmail.com>
2025-08-03 13:53:14 -0700

    added the layouts so it can be shown the ai_form  site

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>AI Blog Generator</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body class="font-sans antialiased">
+    <div class="container mx-auto p-6">
+        @yield('content')
+    </div>
+</body>
+</html>
